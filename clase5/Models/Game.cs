bf125899a499932bef4aba5038a27b526ff27f51
Walter <walter_orellanam@icloud.com>
2024-03-03 03:04:08 -0300

    add clase5

---
+++ clase5/Models/Game.cs
@@ -0,0 +1,12 @@
+namespace clase5.Models;
+
+public class Game
+{
+    public int Id {get;set;}
+    public string Name { get; set; }
+    public int Release { get; set; }
+    public string Gender { get; set; }
+    public bool IsMultiplayer { get; set; }
+    public decimal Price { get; set; }
+    public string Company {get; set; }
+}
